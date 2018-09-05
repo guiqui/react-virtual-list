391340ff3a4b5a9f4357517c4b634dc6e4d82e5c
Guillermo Quiros <gquiman@gmail.com>
2018-09-05 16:40:17 +1200

    Adding codecov

--- jest.config.js
+++ jest.config.js
@@ -5,8 +5,8 @@ module.exports = {
         "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/__mocks__/fileMock.js",
         "\\.(css|less)$": "identity-obj-proxy"
     },
+    coverageDirectory: "./coverage/",
     collectCoverage:true,
     coverageReporters: ["lcov", "text"],
-    setupFiles:["./src/test/TestSetup.js"]//,
-    //reporters=["jest-junit"]
+    setupFiles:["./src/test/TestSetup.js"]
 };
